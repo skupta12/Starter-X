e023f481e5baf11d114c9f91a4dde8abe548e808
skupta12 <tosya2001@gmail.com>
2024-04-15 12:17:51 +0300

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+				port: "3000"
+			},
+			{
+				protocol: "https",
+				port: "",
+				hostname: "starter-x.vercel.app",
+				pathname: "/**"
+			},
+		],
+	},
+};
 
 export default nextConfig;
