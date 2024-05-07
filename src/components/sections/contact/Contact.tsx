1189dbbd1c9c25a6937ec088745c80d4c2fedd32
skupta12 <tosya2001@gmail.com>
2024-05-07 20:59:25 +0300

    button types

--- src/components/sections/contact/Contact.tsx
+++ src/components/sections/contact/Contact.tsx
@@ -10,19 +10,13 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { schema } from "@/lib/validators";
 import { z } from "zod";
 import { useState } from "react";
+import { errorMessages } from "@/lib/validators";
 
 type FormFields = z.infer<typeof schema>;
 
 const Contact = () => {
-
+  
   const [isFormSubmitted, setIsFormSubmitted] = useState<boolean>(false);
-
-  const errorMessages = {
-    email: "Please enter a valid email address.",
-    name: "Please enter a name with at least 2 characters.",
-    phone: "Please enter a valid phone number.",
-    // services: "Please enter a services with at least 3 character",
-  };
 
   const {
     register,
@@ -42,8 +36,7 @@ const Contact = () => {
 
       console.log(data);
 
-      setIsFormSubmitted(true)
-
+      setIsFormSubmitted(true);
     } catch (error) {
       setError("root", {
         message: "This email is already taken",
@@ -102,11 +95,13 @@ const Contact = () => {
               className="grid grid-cols-12 sm:gap-8 gap-6 sm:mb-0 mb-10"
             >
               {contact.map((item, i) => (
+                
                 <div
                   className="sm:col-span-6 
                 col-span-12"
                   key={i}
                 >
+                  
                   <input
                     {...register(item.name as keyof FormFields)}
                     placeholder={item.placeholder}
@@ -114,36 +109,52 @@ const Contact = () => {
                     outline-none sm:text-[18px] p-5 text-black
             border bg-transparent focus:text-black border-primary-100"
                   />
+                  {/* @ts-expect-error */}
                   {errors[item.name] && (
-                        <div className="text-red-500 text-[14px] mt-2">
-                            {errorMessages[item.name]}
-                        </div>
-                    )}
+                    <div className="text-red-500 text-[14px] mt-2">
+                      {/* @ts-expect-error */}
+                      {errorMessages[item.name]}
+                    </div>
+                  )}
                 </div>
               ))}
 
               <div className="col-span-12">
                 <textarea
-                {...register("text")}
+                  {...register("text")}
                   className="w-full bg-transparent border border-primary-100 outline-none 
                   p-5 placeholder:text-primary-100 sm:text-[18px] resize-y"
                   placeholder="Write your message here..."
                   rows={5}
-                /> 
+                />
               </div>
 
               <div className="col-span-12">
                 <button
                   disabled={isSubmitting}
                   type="submit"
-                  className={`${isSubmitting ? "bg-primary-200" : "bg-primary-100"} sm:w-[270px] h-[65px] w-full sm:text-[18px] font-medium px-[30px] 
+                  className={`${
+                    isSubmitting ? "bg-primary-200" : "bg-primary-100"
+                  } sm:w-[270px] h-[65px] w-full sm:text-[18px] font-medium px-[30px] 
                   py-[18px] inline-block hover:scale-95 duration-300 cursor-pointer 
                   border border-primary-100 text-white`}
                 >
-                  {isSubmitting ? <div><LoaderCircle className="animate-spin text-center mx-auto text-white "/></div> : "Send a message"}
+                  {isSubmitting ? (
+                    <div>
+                      <LoaderCircle className="animate-spin text-center mx-auto text-white " />
+                    </div>
+                  ) : (
+                    "Send a message"
+                  )}
                 </button>
-                {errors.root && <div className="text-red-500">{errors.root.message}</div>}
-                {isFormSubmitted ? <div className="text-red-500 text-[14px] mt-2">Disabled</div> : ""}
+                {errors.root && (
+                  <div className="text-red-500">{errors.root.message}</div>
+                )}
+                {isFormSubmitted ? (
+                  <div className="text-red-500 text-[14px] mt-2">Disabled</div>
+                ) : (
+                  ""
+                )}
               </div>
             </form>
           </OpacityTransition>
--- src/lib/validators.ts
+++ src/lib/validators.ts
@@ -7,3 +7,10 @@ export const schema = z.object({
     services: z.string().optional(),
     text: z.string().optional()
   });
+
+  export const errorMessages = {
+    email: "Please enter a valid email address.",
+    name: "Please enter a name with at least 2 characters.",
+    phone: "Please enter a valid phone number.",
+    // services: "Please enter a services with at least 3 character",
+  };
